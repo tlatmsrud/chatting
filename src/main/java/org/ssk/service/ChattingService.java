c077ddb2bb5223802f56d2d51aa5233bc7ebc816
tlatmsrud <tlatmsrud@naver.com>
2023-07-20 20:13:52 +0900

    :art: Add-getChattingRoom-Method

--- src/main/java/org/ssk/service/ChattingService.java
+++ src/main/java/org/ssk/service/ChattingService.java
@@ -1,7 +1,11 @@
 package org.ssk.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.ssk.dto.ChattingRoomDto;
+import org.ssk.entity.ChattingRoomEntity;
+import org.ssk.repository.ChattingRoomRepository;
 
 /**
  * title        :
@@ -11,10 +15,41 @@ import org.ssk.dto.ChattingRoomDto;
  */
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ChattingService {
 
-    public Long createChattingRoom(ChattingRoomDto.Create request){
-        return null;
+    private final ChattingRoomRepository chattingRoomRepository;
+
+    /**
+     * 채팅방 조회하기
+     * @param request - 채팅방 조회 요청 Dto
+     * @return 조회 또는 생성된 채팅방 ID
+     */
+    public Long getChattingRoom(ChattingRoomDto request){
+
+        Long roomId = findChattingRoom(request.getFromMemberId(), request.getToMemberId());
+
+        if(roomId != null){
+            return roomId;
+        }
+
+        ChattingRoomEntity chattingRoom = ChattingRoomEntity.builder()
+            .fromMemberId(request.getFromMemberId())
+            .toMemberId(request.getToMemberId())
+            .build();
+
+        return chattingRoomRepository.save(chattingRoom).getId();
+    }
+
+    /**
+     * 채팅방 찾기
+     * @param fromMemberId - 본인 유저 ID
+     * @param toMemberId - 상대방 유저 ID
+     * @return 채팅방 ID
+     */
+    public Long findChattingRoom(Long fromMemberId, Long toMemberId){
+        return chattingRoomRepository.findIdByMemberId(fromMemberId, toMemberId);
     }
 
 }
