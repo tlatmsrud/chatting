05d9972fdb5fef97b963b1aa6f6d9ea36bf14227
tlatmsrud <tlatmsrud@naver.com>
2023-07-20 23:58:47 +0900

    :coffin:

--- src/main/java/org/ssk/config/SecurityConfig.java
+++ src/main/java/org/ssk/config/SecurityConfig.java
@@ -1,24 +1,17 @@
 package org.ssk.config;
 
 import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 import org.ssk.filter.JwtAuthorizationFilter;
 import org.ssk.oauth.CustomSuccessHandler;
 import org.ssk.provider.JwtProvider;
 import org.ssk.repository.MemberRepository;
-import org.ssk.service.TokenService;
 
 /**
  * title        :
@@ -28,7 +21,7 @@ import org.ssk.service.TokenService;
  */
 @Configuration
 @AllArgsConstructor
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfig{
 
     private final JwtProvider jwtProvider;
@@ -37,7 +30,7 @@ public class SecurityConfig{
 
     // HttpServletRequest에 대한 필터체인을 정의
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector ) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http ) throws Exception{
 
         http
                 .authorizeHttpRequests((authz) -> authz
@@ -58,5 +51,4 @@ public class SecurityConfig{
 
         return http.build();
     }
-
 }
