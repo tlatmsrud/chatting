0f3cb289672eaf10e12c87bb684ec3ba4dd51de9
tlatmsrud <tlatmsrud@naver.com>
2023-07-20 20:14:15 +0900

    :white_check_mark: Test

---
+++ src/test/java/org/ssk/controller/ChattingControllerTest.java
@@ -0,0 +1,61 @@
+package org.ssk.controller;
+
+import attributes.TestFixture;
+import attributes.TestMemberAuth;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.ssk.dto.ChattingRoomDto;
+import org.ssk.service.ChattingService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-20
+ * description  :
+ */
+
+@WebMvcTest(ChattingController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@Import(HttpEncodingAutoConfiguration.class)
+class ChattingControllerTest implements TestFixture {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ChattingService chattingService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp(){
+        given(chattingService.getChattingRoom(any(ChattingRoomDto.class)))
+                .willReturn(CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER);
+
+    }
+    @Test
+    @TestMemberAuth
+    void createChattingRoom() throws Exception {
+        mockMvc.perform(
+                post("/api/chatting/room")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(CHATTING_ROOM_DTO))
+                        .with(csrf())
+        ).andExpect(status().isCreated());
+    }
+}
---
+++ src/test/java/org/ssk/service/ChattingServiceTest.java
@@ -0,0 +1,48 @@
+package org.ssk.service;
+
+import attributes.TestFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.ssk.entity.ChattingRoomEntity;
+import org.ssk.repository.ChattingRoomRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-07-20
+ * description  :
+ */
+class ChattingServiceTest implements TestFixture{
+
+    private ChattingRoomRepository chattingRoomRepository = mock(ChattingRoomRepository.class);
+
+    private final ChattingService chattingService = new ChattingService(chattingRoomRepository);
+
+    @BeforeEach
+    void setUp(){
+        given(chattingRoomRepository.findIdByMemberId(LOGIN_MEMBER_ID, VALID_MEMBER_ID))
+                .willReturn(CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER);
+
+        given(chattingRoomRepository.save(any(ChattingRoomEntity.class)))
+                .willReturn(CHATTING_ROOM_ENTITY);
+    }
+    @Test
+    @DisplayName("채팅방 조회")
+    void getChattingRoom() {
+        assertThat(chattingService.getChattingRoom(CHATTING_ROOM_DTO))
+                .isEqualTo(CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER);
+    }
+
+    @Test
+    @DisplayName("채팅방 찾기")
+    void findChattingRoom() {
+        assertThat(chattingService.findChattingRoom(LOGIN_MEMBER_ID, VALID_MEMBER_ID))
+                .isEqualTo(CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER);
+    }
+}
