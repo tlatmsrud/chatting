50b2a22919db9f83cf2b582b4dc0b608b5b6b9c9
tlatmsrud <tlatmsrud@naver.com>
2023-07-20 20:12:53 +0900

    :art: Add-Fixture

--- src/test/java/attributes/TestFixture.java
+++ src/test/java/attributes/TestFixture.java
@@ -1,6 +1,7 @@
 package attributes;
 
 import org.ssk.dto.ChattingRoomDto;
+import org.ssk.entity.ChattingRoomEntity;
 
 /**
  * title        : Text Fixture
@@ -11,6 +12,8 @@ import org.ssk.dto.ChattingRoomDto;
 public interface TestFixture {
     Long LOGIN_MEMBER_ID = 1L;
     Long VALID_MEMBER_ID = 2L;
-    ChattingRoomDto.Create createChattingRoomDto = new ChattingRoomDto.Create(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
+    ChattingRoomDto CHATTING_ROOM_DTO = new ChattingRoomDto(LOGIN_MEMBER_ID, VALID_MEMBER_ID);
     Long CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER = 1L;
+    ChattingRoomEntity CHATTING_ROOM_ENTITY = new ChattingRoomEntity(CHATTING_ROOM_ID_FOR_LOGIN_MEMBER_AND_VALID_MEMBER
+            , LOGIN_MEMBER_ID, VALID_MEMBER_ID);
 }
