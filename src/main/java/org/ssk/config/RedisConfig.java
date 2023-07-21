a59725c9e9c6cc609e270fc5d40ee8152bf4d281
tlatmsrud <tlatmsrud@naver.com>
2023-07-21 16:26:00 +0900

    :art: Update-Redis-ObjectMapper
    레디스에서 데이터 조회 시 내부적으로 objectMapper를 사용하는데, 기본 설정에 따라 Json 파싱한 데이터 타입을 LinkedHashMap으로 가져오고 있었고, 리스트의 값을 ChatDTO로 캐스팅하는 과정에서 Type관련 예외가 발생함. 이를 해결하기 위한 구문을 추가함.

--- src/main/java/org/ssk/config/RedisConfig.java
+++ src/main/java/org/ssk/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package org.ssk.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +12,8 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.ssk.dto.ChatDto;
+
 
 
 /**
@@ -31,11 +37,20 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(){
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, ChatDto> redisTemplate(){
+        PolymorphicTypeValidator typeValidator = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Object.class)
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(typeValidator, ObjectMapper.DefaultTyping.NON_FINAL);
+        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        RedisTemplate<String, ChatDto> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(valueSerializer);
         return redisTemplate;
     }
 }
